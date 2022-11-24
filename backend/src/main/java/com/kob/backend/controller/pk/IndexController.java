eac435811704b69f5baceb567ae81a3b98369415
csa <csa@37.com>
2022-11-24 21:04:57 +0800

    实现匹配系统微服务

--- backend/src/main/java/com/kob/backend/controller/pk/IndexController.java
+++
@@ -1,8 +0,0 @@
-package com.kob.backend.controller.pk;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class IndexController {
-
-}
---
+++ backendcloud/matchingsystem/src/main/java/com/kob/matchingsystem/service/impl/utills/MatchingPool.java
@@ -0,0 +1,111 @@
+package com.kob.matchingsystem.service.impl.utills;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.locks.ReentrantLock;
+@Component
+public class MatchingPool extends Thread {
+    private static List<Player> players = new ArrayList<>();
+    private final ReentrantLock lock = new ReentrantLock();
+    private final static String startGameUrl = "http://127.0.0.1:3000/pk/start/game/";
+    private static RestTemplate restTemplate;
+
+    @Autowired
+    public void setRestTemplate(RestTemplate restTemplate) {
+        MatchingPool.restTemplate = restTemplate;
+    }
+
+    public void addPlayer(Integer userId, Integer rating) {
+        lock.lock();
+        try {
+            Player player = new Player(userId, rating, 0);
+            players.add(player);
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    public void removePlayer(Integer userId) {
+        List<Player> newPlayers = new ArrayList<>();
+        lock.lock();
+        try {
+            for (Player player : players) {
+                if (!player.getUserId().equals(userId)) {
+                    newPlayers.add(player);
+                }
+            }
+            players = newPlayers;
+        } finally {
+            lock.unlock();
+        }
+    }
+
+    private void increaseWaitingTime() {
+        for (Player player : players) {
+            player.setWaitingTime(player.getWaitingTime() + 1);
+        }
+    }
+
+    private boolean checkMatched(Player a, Player b) {
+        Integer ratingDelta = Math.abs(a.getRating() - b.getRating());
+        Integer waitingTime = Math.min(a.getWaitingTime(), b.getWaitingTime());
+        return ratingDelta <= waitingTime * 10;
+    }
+
+    private void sendResult(Player a, Player b) {
+        System.out.println("send result " + a + ' ' + b);
+        MultiValueMap<String, String> data = new LinkedMultiValueMap<>();
+        data.add("a_id", a.getUserId().toString());
+        data.add("b_id", b.getUserId().toString());
+        restTemplate.postForObject(startGameUrl, data, String.class);
+    }
+
+    private void matchPlayers() {
+        System.out.println("match players: " + players.toString());
+        boolean[] used = new boolean[players.size()];
+        for (int i = 0; i < players.size(); i++) {
+            if (used[i]) continue;
+            for (int j = i + 1; j < players.size(); j++) {
+                if (used[j]) continue;
+                Player a = players.get(i), b = players.get(j);
+                if (checkMatched(a, b)) {
+                    used[i] = used[j] = true;
+                    sendResult(a, b);
+                    break;
+                }
+            }
+        }
+        List<Player> newPlayers = new ArrayList<>();
+        for (int i = 0; i < players.size(); i++) {
+            if(!used[i]) {
+                newPlayers.add(players.get(i));
+            }
+        }
+        players = newPlayers;
+    }
+
+    @Override
+    public void run() {
+        while (true) {
+            try {
+                Thread.sleep(1000);
+                lock.lock();
+                try {
+                    increaseWaitingTime();
+                    matchPlayers();
+                } finally {
+                    lock.unlock();
+                }
+            } catch (InterruptedException e) {
+                 e.printStackTrace();
+                 break;
+            }
+        }
+    }
+}
